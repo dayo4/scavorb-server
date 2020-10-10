b4dfac51406fc44a23e1f37712f9691cb350adab
dayo4 <dayo4@live.com>
2020-10-10 01:45:11 +0100

    .

--- src/public/js/chunk-2d0b6179.186912af.js
+++ src/public/js/chunk-2d0b6179.186912af.js
@@ -1,2 +1,1 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b6179"],{"1c4e":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("router-view")],1)},c=[],r=t("2877"),l={},i=Object(r["a"])(l,u,c,!1,null,null,null);n["default"]=i.exports}}]);
-//# sourceMappingURL=chunk-2d0b6179.186912af.js.map
